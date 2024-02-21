0776a7734b5369ff0cb06aa777cfd0190a830147
god-lamp <2545977650@qq.com>
2024-02-22 00:37:07 +0800

    feat:系统页面请求接口

---
+++ src/api/sys/account.js
@@ -0,0 +1,42 @@
+import request from '@/utils/request'
+
+export function fetchAccounts(query) {
+  return request({
+    baseURL: 'http://localhost:19999/api/devops',
+    url: '/account',
+    method: 'get',
+    params: query
+  })
+}
+
+export function fetchArticle(id) {
+  return request({
+    url: '/vue-element-admin/article/detail',
+    method: 'get',
+    params: { id }
+  })
+}
+
+export function fetchPv(pv) {
+  return request({
+    url: '/vue-element-admin/article/pv',
+    method: 'get',
+    params: { pv }
+  })
+}
+
+export function createArticle(data) {
+  return request({
+    url: '/vue-element-admin/article/create',
+    method: 'post',
+    data
+  })
+}
+
+export function updateArticle(data) {
+  return request({
+    url: '/vue-element-admin/article/update',
+    method: 'post',
+    data
+  })
+}
